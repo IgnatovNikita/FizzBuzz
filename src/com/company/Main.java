c131a8efd9f9ecd1ac70259611b9d1ec2afaf091
IgnatovNikita <star08409@gmail.com>
2022-07-26 21:34:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static List<String> fizzBuzz(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++ ){
+            if (i % 15 == 0) res.add("FizzBuzz");
+            else if ( i % 5 == 0) res.add("Buzz");
+            else if ( i % 3 == 0) res.add("Fizz");
+            else res.add(String.valueOf(i));
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(fizzBuzz(15));
+    }
+}
